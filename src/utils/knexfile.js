447ceca8add86f11988921d5506337e99da1c8c9
Jinoo Lee <etture@gmail.com>
2018-11-29 22:13:45 +0900

    commented out knexfile dependency on production mode

--- src/utils/knexfile.js
+++ src/utils/knexfile.js
@@ -1,15 +1,21 @@
 // const connection = process.env.RDS_URL || require('../local/mysql_connection');
-let connection;
-if (process.env.NODE_ENV === 'production') {
-    connection = {
-        host: process.env.RDS_HOST,
-        user: process.env.RDS_USER,
-        password: process.env.RDS_PASSWORD,
-        database: process.env.RDS_DATABASE
-    };
-} else {
-    connection = require('../local/mysql_connection');
-}
+// let connection;
+// if (process.env.NODE_ENV === 'production') {
+//     connection = {
+//         host: process.env.RDS_HOST,
+//         user: process.env.RDS_USER,
+//         password: process.env.RDS_PASSWORD,
+//         database: process.env.RDS_DATABASE
+//     };
+// } else {
+//     connection = require('../local/mysql_connection');
+// }
+const connection = {
+    host: process.env.RDS_HOST,
+    user: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DATABASE
+};
 
 
 const knex = require('knex')({
