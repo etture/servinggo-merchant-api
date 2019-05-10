159531d689d286f1373289e51c5181b2bf34005b
Jinoo Lee <etture@gmail.com>
2019-05-10 21:25:59 +0900

    edited for deployment

--- src/utils/knexfile.js
+++ src/utils/knexfile.js
@@ -14,7 +14,8 @@ const connection = {
     host: process.env.RDS_HOST,
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DATABASE
+	database: process.env.RDS_DATABASE,
+	port: 3306
 };
 
 
